374f2255ecf6d90d1c31570f7ffab270ddc4304d
giorgi radicalloop <giorgi.gujejiani.1@iliauni.edu.ge>
2023-08-04 00:53:04 +0400

    general styles changed.

--- app/components/Joinus/index.tsx
+++ app/components/Joinus/index.tsx
@@ -5,9 +5,9 @@ const Join = () => {
             <div className='mx-auto max-w-2xl lg:max-w-7xl sm:py-4 lg:px-8'>
 
                 <div className="text-center">
-                    <h3 className="text-blue text-lg font-normal tracking-widest">JOIN US</h3>
-                    <h2 className="text-4xl sm:text-6xl font-bold my-6 leading-10"> Take your business to <br /> the new level.</h2>
-                    <p className="text-lightblack text-base font-normal">Craven omni memoria patriae zombieland clairvius narcisse religionis sunt diri undead <br /> historiarum. Golums, zombies unrelenting et Raimi fascinati beheading.</p>
+                    <h3 className="text-white text-lg font-normal tracking-widest">JOIN US</h3>
+                    <h2 className="text-4xl text-white sm:text-6xl font-bold my-6 leading-10"> Take your business to <br /> the new level.</h2>
+                    <p className="text-lightgrey text-base font-normal">Craven omni memoria patriae zombieland clairvius narcisse religionis sunt diri undead <br /> historiarum. Golums, zombies unrelenting et Raimi fascinati beheading.</p>
                 </div>
 
                 <div className="mx-auto max-w-4xl pt-5">
