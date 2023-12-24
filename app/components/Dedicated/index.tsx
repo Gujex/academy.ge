221a75d45d6acaaec6f221a8f0a67e1303ca1ea6
Dato Tchkadua <dato.tchkadua.35@gmail.com>
2023-12-24 21:23:17 +0400

    check

--- app/components/Dedicated/index.tsx
+++ app/components/Dedicated/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Dedicated = () => {
     return (
         <div className="relative">
-
+{/* hjh */}
             <Image src="/images/dedicated/spiral.svg" height={272} width={686} alt="spiral-design" className="absolute left-0 hidden lg:block -z-10" />
 
             <div className='mx-auto max-w-7xl px-4 my-40 sm:py-20 lg:px-8'>
